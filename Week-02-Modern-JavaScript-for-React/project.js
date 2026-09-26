560da22c017de4144f4b306a2392c2d7715f225d
Jithin <jithinvenugopaltz@gmail.com>
2026-09-26 14:06:24 +0530

    Add detailed explanations for map and filter functions

---
+++ Week-02-Modern-JavaScript-for-React/project.js
@@ -0,0 +1,9 @@
+const students = [
+  { id: 1, name: "Aarav Sharma", mark: 92, stream: "Science" },
+  { id: 2, name: "Diya Nair", mark: 76, stream: "Commerce" },
+  { id: 3, name: "Kavya Patel", mark: 88, stream: "Science" },
+  { id: 4, name: "Mohammed Zayd", mark: 64, stream: "Humanities" },
+  { id: 5, name: "Rithvik Sen", mark: 95, stream: "Science" }
+];
+
+const distinctionStudents = students.filter()
